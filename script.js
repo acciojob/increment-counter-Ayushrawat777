f4c316d05bfecde885bb32ebd627ba3db3e0d703
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-03-31 13:53:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+   let count = 0;
+      const handleIncrement = () => {
+        count++;
+        alert(count)
+        document.getElementById("total-count").innerHTML = count;
+      };
